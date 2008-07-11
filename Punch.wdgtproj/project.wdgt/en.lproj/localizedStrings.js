698464fd31368a64b4b52b3050327aeeb4fd0046
Yossef Mendelssohn <ymendel@pobox.com>
2008-07-11 03:15:27 -0500

    Blanking out some default text and removing some localization keys.

--- Punch.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
+++ Punch.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
@@ -1,5 +1,3 @@
 var localizedStrings = new Object;
 
-localizedStrings["Hello, World!"] = unescape('punch%2C yo');
 localizedStrings["Done"] = "Done";
-localizedStrings['punch title'] = unescape('punch%2C yo');
