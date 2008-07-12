5faf058dba9daeec89c1a0eea82a9b048c961c95
Yossef Mendelssohn <ymendel@pobox.com>
2008-07-11 22:05:04 -0500

    Updating deployed widget.

--- Punch.wdgt/Parts/setup.js
+++ Punch.wdgt/Parts/setup.js
@@ -5,9 +5,9 @@ function setupParts() {
     setupParts.called = true;
     CreateInfoButton('info', { frontID: 'front', foregroundStyle: 'white', backgroundStyle: 'black', onclick: 'showBack' });
     CreateGlassButton('done', { text: 'Done', onclick: 'showFront' });
-    CreateText('project_name_display', {  });
+    CreateText('project_name_display', { text: 'project name' });
     CreateText('project_name_input_label', { text: 'Project Name:' });
     CreateIndicator('project_status_indicator', { onValue: 1, criticalValue: 3, warningValue: 2 });
-    CreateButton('punch_button', { leftImageWidth: 10, onclick: 'buttonAction', rightImageWidth: 10 });
+    CreateButton('punch_button', { onclick: 'buttonAction', rightImageWidth: 10, leftImageWidth: 10 });
 }
 window.addEventListener('load', setupParts, false);
